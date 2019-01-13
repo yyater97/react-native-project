b489e3c1f850c88e7a055c98381328c7d1916f00
TranHungQuang <tranhunquang23497@gmail.com>
2019-01-14 01:44:34 +0700

    add panorama for react-native

--- android/app/src/main/java/com/busmap/MainApplication.java
+++ android/app/src/main/java/com/busmap/MainApplication.java
@@ -3,6 +3,7 @@ package com.busmap;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.xebia.googlevrpanorama.RNGoogleVRPanoramaPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGoogleVRPanoramaPackage(),
             new VectorIconsPackage(),
             new SvgPackage(),
             new RNGestureHandlerPackage(),
